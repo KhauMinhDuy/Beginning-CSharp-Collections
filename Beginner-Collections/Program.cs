d918797b4f078902d0145c22fae687f51e098448
khauminhduy <khauminhduy@gmail.com>
2022-05-26 23:32:28 +0700

    init

---
+++ Beginner-Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Beginner_Collections
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
